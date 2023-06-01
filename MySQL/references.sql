c655566ec36a971acc1a001c466f7146a6d8ba12
Lucas Quintino <lucas.quintino@pontoid.com.br>
2023-06-01 09:56:12 -0300

    reference for mysql commands

---
+++ MySQL/references.sql
@@ -0,0 +1,4 @@
+SHOW MASTER STATUS; -- Log em uso
+SHOW SLAVE STATUS; --
+SHOW PROCESSLIST; -- Lista de instancias
+PURGE BINARY LOGS TO 'DESKTOP-AQ06847-bin.000015'; -- Excluir binarios anteriores ao selecionado
