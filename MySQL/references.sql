ee79fac7c7d11f859acd428622d262f11d678805
Lucas Quintino <lucas.quintino@pontoid.com.br>
2023-06-02 17:18:59 -0300

    alter table and update column

--- MySQL/references.sql
+++ MySQL/references.sql
@@ -1,4 +1,7 @@
+-- MY SQL VERSION 8.0.29 --
 SHOW MASTER STATUS; -- Log em uso
 SHOW SLAVE STATUS; --
 SHOW PROCESSLIST; -- Lista de instancias
 PURGE BINARY LOGS TO 'DESKTOP-AQ06847-bin.000015'; -- Excluir binarios anteriores ao selecionado
+ALTER TABLE nome_tabela CHANGE nome_antigo novo_nome INT; -- Alterar o nome de uma coluna
+UPDATE nome_tabela SET nome_coluna = valor WHERE condicao; -- Atualizar valores de uma coluna
